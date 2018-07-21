92f91d65fdc6430d05ef58eb1d1fa27202dc3f62
ghigbie <gehigbie@gmail.com>
2018-07-20 23:36:56 -0600

    Add containing conditional

--- bouncer.py
+++ bouncer.py
@@ -1,9 +1,12 @@
 age = int(input("How old are you? Please enter your age: "))
 
-if age >= 18:
-    if age >= 21:
-        print('You can drink and watch!')
+if age != ""
+    if age >= 18:
+        if age >= 21:
+            print('You can drink and watch!')
+        else:
+            print('You can only watch')
     else:
-        print('You can only watch')
+        print('Go away youngster!')
 else:
-    print('Go away youngster!')
+    print("Please enter an age")
