6eec31454566cc2a7c32f59d46c3c212ff0a1b2d
ghigbie <gehigbie@gmail.com>
2018-07-20 23:31:29 -0600

    Add bouncer file

---
+++ bouncer.py
@@ -0,0 +1,9 @@
+age = input("Please type your age.")
+
+if age >= 18:
+    if age >= 21:
+        print('You can drink and watch!')
+    else:
+        print('You can only watch')
+else:
+    print('Go away youngster!')
