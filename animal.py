ceaff4bd973258e80dd0e99087d4c3909e44dff5
ghigbie <gehigbie@gmail.com>
2018-07-20 21:48:58 -0600

    Edit  animal game

--- animal.py
+++ animal.py
@@ -2,4 +2,5 @@ animal = input("Please enter your favorite animal.")
 if animal:
     print(f"{animal} is my favorite too!")
 else:
+    print("You didn't say anything!!!")
     animal = input("Please enter your favorite animal")
