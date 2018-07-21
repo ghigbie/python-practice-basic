766f27cd27a03acb836f3ba0a957f52a0cba414d
ghigbie <gehigbie@gmail.com>
2018-07-20 21:22:30 -0600

    Edit f strings

--- mileage.py
+++ mileage.py
@@ -4,4 +4,4 @@ conversion = 1.60934
 print(f"Ok, you said {kms}!")
 miles = round(float(kms)/conversion, 2)
 print(f"That is {miles} miles! Nice work!")
-print(f"You {kms}km ride is {miles}mi")
+print(f"You {kms} km ride is {miles} mi")
