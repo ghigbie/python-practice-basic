db8e7deb0b39a7388bd796bacf17f67fdaaf53d3
ghigbie <gehigbie@gmail.com>
2018-07-21 17:57:37 -0600

    Create basic rock paper game

---
+++ rock-paper-scissors.py
@@ -0,0 +1,26 @@
+print("...rock...")
+print("...paper...")
+print("...scissors...")
+player1 = input("Player 1, please choose one of the three choices: ").lower()
+count = 0
+while count < 20:
+    print("* * * NO CHEATING * * *")
+    count++
+player2 = input("Player 2, please choose one of the three choices: ").lower()
+
+win1 = "Player 1 wins!"
+win2 = "Player 2 wins!"
+if player1 == 'rock' and player2 == 'paper':
+    print(win1)
+elif player1 == 'paper' and player2 == 'rock':
+    print(win1)
+elif player1 == 'scissors' and player2 == 'paper':
+    print(win1)
+if player2 == 'rock' and player1 == 'paper':
+    print(win2)
+elif player2 == 'paper' and player1== 'rock':
+    print(win2)
+elif player2 == 'scissors' and player1 == 'paper':
+    print(win2)
+else:
+    print('Invalid choice. Please play again.')
