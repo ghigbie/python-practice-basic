58627caa3e0303a8a52ea41463c216296c65f118
ghigbie <gehigbie@gmail.com>
2018-07-21 19:24:32 -0600

    Fix bug

--- unlucky-numbers.py
+++ unlucky-numbers.py
@@ -4,7 +4,7 @@ numbers = range(1, 21)
 for number in numbers:
     if number == 4 or number == 13:
         print(f"{number} is UNLUCKY")
-    elif number % 2 == 1:
+    elif number % 2 == 0:
         print(f"{number} is even")
     else:
         print(f"{number} is odd")
