a29f3b8437f39e8e859179f6fe2d8e2a1af19e5b
ghigbie <gehigbie@gmail.com>
2018-07-21 18:00:15 -0600

    Fix game logic

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -10,13 +10,13 @@ player2 = input("Player 2, please choose one of the three choices: ").lower()
 
 win1 = "Player 1 wins!"
 win2 = "Player 2 wins!"
-if player1 == 'rock' and player2 == 'paper':
+if player1 == 'rock' and player2 == 'scissors':
     print(win1)
 elif player1 == 'paper' and player2 == 'rock':
     print(win1)
 elif player1 == 'scissors' and player2 == 'paper':
     print(win1)
-elif player2 == 'rock' and player1 == 'paper':
+elif player2 == 'rock' and player1 == 'scissors':
     print(win2)
 elif player2 == 'paper' and player1== 'rock':
     print(win2)
