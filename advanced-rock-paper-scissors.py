8a0f35b97e8cc80106b422db595fe0f8301d3c3d
ghigbie <gehigbie@gmail.com>
2018-07-21 18:05:20 -0600

    Add advanced versio of rock paper

---
+++ advanced-rock-paper-scissors.py
@@ -0,0 +1,28 @@
+print("...rock...")
+print("...paper...")
+print("...scissors...")
+player1 = input("Player 1, please choose one of the three choices: ").lower()
+count = 0
+while count < 20:
+    print("* * * NO CHEATING * * *")
+    count+=1
+player2 = input("Player 2, please choose one of the three choices: ").lower()
+
+win1 = "Player 1 wins!"
+win2 = "Player 2 wins!"
+if player1 == 'rock' and player2 == 'scissors':
+    print(win1)
+elif player1 == 'paper' and player2 == 'rock':
+    print(win1)
+elif player1 == 'scissors' and player2 == 'paper':
+    print(win1)
+elif player2 == 'rock' and player1 == 'scissors':
+    print(win2)
+elif player2 == 'paper' and player1== 'rock':
+    print(win2)
+elif player2 == 'scissors' and player1 == 'paper':
+    print(win2)
+elif player1 == player2:
+    print('There is a tie!')
+else:
+    print('Invalid choice. Please play again.')
