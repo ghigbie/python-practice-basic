1bc883cebb1959058dda97286c014f17c03f98dc
ghigbie <gehigbie@gmail.com>
2018-07-22 19:18:59 -0600

    Add winning line

--- stop-copying-me.py
+++ stop-copying-me.py
@@ -2,4 +2,5 @@ stopLine = 'stop copying me'
 player = input('How\'s it going? ')
 print(player)
 while player != stopLine:
-    player = input(player)
+    player = input(player+ "\n")
+print('UGN FINE YOU WIN')
