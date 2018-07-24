7d032e17cbca8464108a017d66042ab002e4e12a
ghigbie <gehigbie@gmail.com>
2018-07-23 23:07:16 -0600

    Add for loop to game

--- multiple-rps.py
+++ multiple-rps.py
@@ -15,22 +15,23 @@ win1 = "Player 1 wins!"
 win2 = "Computer wins!"
 computer_choice = f"Computer chose {comp_random_option}."
 
-if player1 == comp_random_option:
-    print('There is a tie!')
-elif player1 == 'rock':
-    if comp_random_option == 'scissors':
-        print(computer_choice, win1)
-    elif comp_random_option == 'paper':
-        print(computer_choice, win2)
-elif player1 == 'paper':
-    if comp_random_option == 'scissors':
-        print(computer_choice, win2)
-    elif comp_random_option == 'rock':
-        print(computer_choice, win1)
-elif player1 == 'scissors':
-    if comp_random_option == 'rock':
-        print(computer_choice, win2)
-    elif comp_random_option == 'paper':
-        print(computer_choice, win1)
-else:
-    print('Invalid choice. Please play again.')
+for time in range(3):
+    if player1 == comp_random_option:
+        print('There is a tie!')
+    elif player1 == 'rock':
+        if comp_random_option == 'scissors':
+            print(computer_choice, win1)
+        elif comp_random_option == 'paper':
+            print(computer_choice, win2)
+    elif player1 == 'paper':
+        if comp_random_option == 'scissors':
+            print(computer_choice, win2)
+        elif comp_random_option == 'rock':
+            print(computer_choice, win1)
+    elif player1 == 'scissors':
+        if comp_random_option == 'rock':
+            print(computer_choice, win2)
+        elif comp_random_option == 'paper':
+            print(computer_choice, win1)
+    else:
+        print('Invalid choice. Please play again.')
