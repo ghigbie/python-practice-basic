4cbeeda7f35abe92193f6b96d9bc07e48b57ecd4
ghigbie <gehigbie@gmail.com>
2018-07-23 23:15:29 -0600

    Add scoring mechanism

--- multiple-rps.py
+++ multiple-rps.py
@@ -1,37 +1,47 @@
 from random import randint
-
 options = ['rock', 'paper', 'scissors']
-for option in options:
-    print(f"...{option}...")
+player_wins = 0
+computer_wins = 0
 
-comp_random_option = options[randint(0,2)]
+while player_wins < 2 or computer_wins < 2:
+    for option in options:
+        print(f"...{option}...")
 
-player1 = input("Player 1, please choose one of the three choices: ").lower()
-# while player1 != options[0] or player1 != options[1] or player1 != options[2]:
-#     print("You ented an invalid choice.")
-#     player1 = input("Player 1, please choose one of the three choices: ").lower()
+    comp_random_option = options[randint(0,2)]
 
-win1 = "Player 1 wins!"
-win2 = "Computer wins!"
-computer_choice = f"Computer chose {comp_random_option}."
+    player1 = input("Player 1, please choose one of the three choices: ").lower()
+    # while player1 != options[0] or player1 != options[1] or player1 != options[2]:
+    #     print("You ented an invalid choice.")
+    #     player1 = input("Player 1, please choose one of the three choices: ").lower()
 
-for time in range(3):
+    win1 = "Player 1 wins!"
+    win2 = "Computer wins!"
+    computer_choice = f"Computer chooses {comp_random_option}."
+
     if player1 == comp_random_option:
         print('There is a tie!')
     elif player1 == 'rock':
         if comp_random_option == 'scissors':
             print(computer_choice, win1)
+            player_wins += 1
         elif comp_random_option == 'paper':
             print(computer_choice, win2)
+            computer_wins += 1
     elif player1 == 'paper':
         if comp_random_option == 'scissors':
             print(computer_choice, win2)
+            computer_wins += 1
         elif comp_random_option == 'rock':
             print(computer_choice, win1)
+            player_wins += 1
     elif player1 == 'scissors':
         if comp_random_option == 'rock':
             print(computer_choice, win2)
+            computer_wins += 1
         elif comp_random_option == 'paper':
             print(computer_choice, win1)
+            player_wins += 1
     else:
         print('Invalid choice. Please play again.')
+    
+    print(f'Total score: Player {player_wins} / Computer {computer_wins}')
