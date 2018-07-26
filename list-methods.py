431b56e76aa0fbc185e7ce7c7385f71d2f3dd4bd
ghigbie <gehigbie@gmail.com>
2018-07-25 18:47:07 -0600

    Add comments

--- list-methods.py
+++ list-methods.py
@@ -12,7 +12,7 @@ while count < len(numbers):
 count = 0
 
 print ('**********************')
-
+#append will addd a single item to the end of a list
 numbers.append(6)
 numbers.append(7)
 
@@ -27,7 +27,7 @@ while count < len(numbers):
 count = 0 
 
 print ('**********************')
-
+#extend will add another list to the end of a list
 numbers.extend([8, 9, 10, 11, 12, 13])
 
 for num in numbers:
@@ -36,6 +36,22 @@ for num in numbers:
 print ('**********************') 
 
 while count < len(numbers):
+    print(numbers[count])
+    count += 1
+count = 0
+
+print ('**********************')
+
+#insert will add an item at a specific place in a list
+numbers.insert(3, 'Yo!')
+numbers.insert(7, 'Yo2!')
+
+for num in numbers:
+    print(num)
+
+print ('**********************')
+
+for num in numbers:
     print(numbers[count])
     count += 1
 count = 0
