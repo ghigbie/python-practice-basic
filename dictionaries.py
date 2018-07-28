66043cf92e517b45954c830166a9bbfce7b20c33
ghigbie <gehigbie@gmail.com>
2018-07-27 21:11:47 -0600

    Add new dict

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,8 @@
 rooms = dict(living=True, bath=False)
 rooms2 = {'living': True, 'bath': False}
+kittens = dict(number=2, cute=True, loving=True, color="grey", breed="Nabby")
 print(rooms)
 print(rooms2)
 print(rooms['living'])
 print(rooms2['bath'])
+print(kittens)
