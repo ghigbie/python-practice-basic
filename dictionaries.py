066ade1e850e0bb820c0b530051b11e2bcfdb114
ghigbie <gehigbie@gmail.com>
2018-07-27 21:15:00 -0600

    Add new dict

--- dictionaries.py
+++ dictionaries.py
@@ -1,8 +1,21 @@
 rooms = dict(living=True, bath=False)
 rooms2 = {'living': True, 'bath': False}
+rooms3 = dict(lving=False, bed=True, bath=True, kitchen=True)
 kittens = dict(number=2, cute=True, loving=True, color="grey", breed="Nabby")
 print(rooms)
 print(rooms2)
 print(rooms['living'])
 print(rooms2['bath'])
 print(kittens)
+print('**********************')
+for value in kittens.values():
+    print(value)
+print('**********************')
+for value in rooms.values():
+    print(value)
+print('**********************')
+for value in rooms2.values():
+    print(value)
+print('**********************')
+for value in rooms3.values():
+    print(value)
