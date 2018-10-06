a8532511cd902c15e9207c8cd7eb9d46dc6c26b8
ghigbie <gehigbie@gmail.com>
2018-10-06 16:53:20 -0600

    Add another loop

--- list-work.py
+++ list-work.py
@@ -2,3 +2,16 @@ colors =  ['red', 'magenta', 'yellow', 'orange', 'green']
 
 for color in colors:
     print(color)
+
+numbers = [4, 6, 8, 10, 12, 14 , 16, 18]
+
+for num in numbers:
+    print(num * num)
+
+print('***************************')
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
+
+print('**************************')
